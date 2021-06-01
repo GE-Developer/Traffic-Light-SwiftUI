94c4f3e9ab8431398a91fc1f7eaf960f1fde7e60
Michael Bukhrashvili <on-line-misha@mail.ru>
2021-06-01 21:11:44 +0300

    Initial Commit

---
+++ Traffic Light SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Traffic Light SwiftUI
+//
+//  Created by Mikhail Bukhrashvili on 01.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Traffic Light SwiftUI/Traffic_Light_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_Light_SwiftUIApp.swift
+//  Traffic Light SwiftUI
+//
+//  Created by Mikhail Bukhrashvili on 01.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_Light_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
