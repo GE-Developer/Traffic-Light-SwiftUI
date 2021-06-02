c341733e02c30d5fab29e8cc2cffff58f7ad0bf8
Michael Bukhrashvili <on-line-misha@mail.ru>
2021-06-02 16:01:04 +0300

    Done

--- Traffic Light SwiftUI/ContentView.swift
+++ Traffic Light SwiftUI/ContentView.swift
@@ -8,39 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var redOpacity = 0.5
+    @State private var yellowOpacity = 0.5
+    @State private var greenOpacity = 0.5
+    @State private var buttonText = "START"
+    
     var body: some View {
         ZStack{
             background
             VStack {
                 trafficLight
                 Spacer()
-                nextButton
+                startButton
             }
         }
     }
     
-    private var nextButton: some View {
-        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-            Text("NEXT")
+    private var startButton: some View {
+        Button(action: {changeLights()}) {
+            Text(buttonText)
                 .font(.title)
                 .bold()
                 .foregroundColor(.white)
                 .frame(width: 200, height: 60, alignment: .center)
                 .background(Color.blue)
                 .cornerRadius(20)
-                //.overlay
-                .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                .padding(5)
+                .background(Color.white)
+                .cornerRadius(22)
         }
     }
     
-    
-    
-    
     private var trafficLight: some View {
         VStack {
-            ColorCircle(color: .red, opacity: 0.5)
-            ColorCircle(color: .yellow, opacity: 0.5)
-            ColorCircle(color: .green, opacity: 0.5)
+            ColorCircle(color: .red,
+                        opacity: redOpacity)
+            ColorCircle(color: .yellow,
+                        opacity: yellowOpacity)
+            ColorCircle(color: .green,
+                        opacity: greenOpacity)
         }
     }
     
@@ -48,8 +55,26 @@ struct ContentView: View {
         Color(.black)
             .ignoresSafeArea()
     }
+    
+    private func changeLights() {
+        if buttonText == "START" {
+            buttonText = "NEXT"
+            redOpacity = 1
+            
+        } else if redOpacity == 1 {
+            redOpacity = 0.5
+            yellowOpacity = 1
+            
+        } else if yellowOpacity == 1 {
+            yellowOpacity = 0.5
+            greenOpacity = 1
+            
+        } else if greenOpacity == 1 {
+            greenOpacity = 0.5
+            redOpacity = 1
+        }
+    }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
